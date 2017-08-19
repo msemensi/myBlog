b9ce7f4450f3ee63d358683c4d70c5e496477b26
Mike Semensi <msemensi@gmail.com>
2017-08-19 21:10:31 +0000

    mike

--- hello.rb
+++ hello.rb
@@ -1,2 +1,14 @@
 puts 'hello, MIKE!!'
-puts 'Bring me a sandwich - NOOOOWWW!!'
+puts 'Bring me a sandwich - NOOOOWWW!!'git 
+
+gift = [
+          "half of a leftover burrito",
+          "a day-old cup of coffee",
+          "a tesla",
+          "a life-size quadrocopter",
+          "a slice of stale wedding cake",
+          "a medium-sized island off the coast of Sumatra",
+          "a visual prosthesis",
+          "a house in the Castro,"
+         ]
+  puts "All I got for valentine's day was #{gift.sample}."
